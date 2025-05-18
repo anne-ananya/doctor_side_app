29411d8e81307a6d045ffc5361a8c78d316c3a6a
anne-ananya <124860809+anne-ananya@users.noreply.github.com>
2025-05-19 00:31:16 +0530

    changes in firebase config to not disclose the ids

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -5,13 +5,13 @@ import { getDatabase } from "firebase/database";
 
 // Your Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyCmxusCy3vqWEJ9mrNIa95Z-CfPW1lRKjA",
-  authDomain: "myrealtimedbapp-4061d.firebaseapp.com",
-  databaseURL: "https://myrealtimedbapp-4061d-default-rtdb.firebaseio.com",
-  projectId: "myrealtimedbapp-4061d",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
   storageBucket: "myrealtimedbapp-4061d.appspot.com",
   messagingSenderId: "241666837689",
-  appId: "1:241666837689:web:418aa4da8bbab1dec11f68"
+  appId: ""
 };
 
 // Initialize Firebase
